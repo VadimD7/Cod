b1453d298b8736fd2767094d006659b14e45c038
VadimD7 <89972321+VadimD7@users.noreply.github.com>
2021-12-20 20:43:21 +0200

    Update Js.js

--- Js.js
+++ Js.js
@@ -4,7 +4,7 @@ function fnc(){
     console.log(a2.checked);
     console.log(a3.checked);
     console.log(a4.checked);
-    if (a4.checked){
+    if (a2.checked){
         alert("Right answer");
     } else {
         alert("Wrong answer");
